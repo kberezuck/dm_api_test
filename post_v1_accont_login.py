d1beaf2bd7a920905a4023b50cbbbe1a55f4b7b7
kberezuck <kseniaberezuck@gmail.com>
2023-06-21 22:30:22 +0300

    создание метода создание аккаунта

--- post_v1_accont_login.py
+++ post_v1_accont_login.py
@@ -1,19 +1,18 @@
 import requests
 
-
 url = "http://localhost:5051/v1/account/login"
 
-payload = json.dumps({
-  "login": "<string>",
-  "password": "<string>",
-  "rememberMe": "<boolean>"
-})
-headers = {
-  'X-Dm-Bb-Render-Mode': '<string>',
-  'Content-Type': 'application/json',
-  'Accept': 'text/plain'
-}
 
-response = requests.request("POST", url, headers=headers, data=payload)
+def post_v1_account_login():
+    payload = {
+        "login": "<string>",
+        "password": "<string>",
+        "rememberMe": "<boolean>"
+    }
+    headers = {
+        'X-Dm-Bb-Render-Mode': '<string>',
+        'Content-Type': 'application/json',
+        'Accept': 'text/plain'
+    }
 
-print(response.text)
+    response = requests.request("POST", url, headers=headers, json=payload)
