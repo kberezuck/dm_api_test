4516b978aafe619e091fd5fad7792bbe9ddd2d11
kberezuck <kseniaberezuck@gmail.com>
2023-06-26 15:05:47 +0300

    ДЗ 2. правка Test_token

--- tests/test_put_v1_account_token.py
+++ tests/test_put_v1_account_token.py
@@ -4,7 +4,7 @@ from services.dm_api_account import DmApiAccount
 def test_put_v1_account_token():
 
     api = DmApiAccount(host="http://localhost:5051")
-    
+
     token="123456789"
     response = api.account.put_v1_account_token(token)
 
