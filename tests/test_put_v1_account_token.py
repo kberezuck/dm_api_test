6540cc16b48e60f411a5af6987c578e45c51ca7d
kberezuck <kseniaberezuck@gmail.com>
2023-07-01 20:21:21 +0300

    ДЗ 4. проверки моделей при помощи pydantic

--- tests/test_put_v1_account_token.py
+++ tests/test_put_v1_account_token.py
@@ -1,5 +1,6 @@
 import structlog
 
+from dm_api_account.models.registration_model import RegistrationModel
 from services.dm_api_account import DmApiAccount
 from services.mailhog import MailhogApi
 
@@ -14,13 +15,13 @@ def test_put_v1_account_token():
     mailhog = MailhogApi(host="http://localhost:5025")
     api = DmApiAccount(host="http://localhost:5051")
 
-    #    json = RegistrationModel(
-    #        login="ksb15",
-    #        email="ksb15@mail.ru",
-    #        password="qwerty1234"
-    #    )
+    json = RegistrationModel(
+        login="ksb17",
+        email="ksb17@mail.ru",
+        password="qwerty1234"
+    )
 
-    #    response = api.account.post_v1_account(json=json)
-    #    assert response.status_code == 201, f"Ожидался статус код 201, а фактически {response.status_code}"
+    response = api.account.post_v1_account(json=json)
+    assert response.status_code == 201, f"Ожидался статус код 201, а фактически {response.status_code}"
     token = mailhog.get_token_from_last_email()
     response = api.account.put_v1_account_token(token=token)
