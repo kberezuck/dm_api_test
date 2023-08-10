207fe8f78701c9f5a6ce70c06ce73d9f3ca5d49f
kberezuck <kseniaberezuck@gmail.com>
2023-08-10 20:20:38 +0300

    Client все тесты

--- tests/test_account/test_create_and_activated_user_with_random_params.py
+++ tests/test_account/test_create_and_activated_user_with_random_params.py
@@ -16,13 +16,23 @@ def random_string(begin=1, end=30):
 @allure.suite("Тесты на проверку метода POST(host)/v1/account по рандомным параметрам")
 @allure.sub_suite("Позитивные проверки")
 class TestsPostV1Account:
-    @pytest.mark.parametrize('login, email, password, status_code, check', [
-        ('12', '12@12.ru', '123456', 201, ''),  # Валидные данные
-        ('12', '12@12.ru', random_string(1, 5), 400, {"Password": ["Short"]}),  # Пароль менее либо равен 5 символам
-        ('1', '12@12.ru', '123456', 400, {"Login": ["Short"]}),  # Логин менее 2 символов
-        ('12', '12@', '123456', 400, {"Email": ["Invalid"]}),  # Емейл не содержит доменную часть
-        ('12', '12', '123456', 400, {"Email": ["Invalid"]})  # Емейл не содержит символ @
-    ])
+    random_email = f'{random_string()}@{random_string()}.{random_string()}'
+    valid_login = random_string(2)
+    invalid_login = random_string(1, 1)
+    valid_password = random_string(6)
+    invalid_password = random_string(1, 5)
+    invalid_email = f'{random_string(6)}'
+    invalid_email_2 = random_string(1, 2).replace('@', '')
+
+    random_data = [
+        (valid_login, random_email, valid_password, 201, ''),
+        (valid_login, random_email, invalid_password, 400, {"Password": ["Short"]}),
+        (invalid_login, random_email, valid_password, 400, {"Login": ["Short"]}),
+        (valid_login, invalid_email, valid_password, 400, {"Email": ["Invalid"]}),
+        (valid_login, invalid_email_2, valid_password, 400, {"Email": ["Invalid"]})
+    ]
+
+    @pytest.mark.parametrize('login, email, password, status_code, check', random_data)
     @allure.title("Создание пользователя по рандомным параметрам и его активация")
     def test_create_and_activated_user_with_random_params(self,
                                                           dm_api_facade,
